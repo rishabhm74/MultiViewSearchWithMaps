eda928077edf1235b8b40a8fabe42c5863aed1c6
rishabh <rishabhm1974@gmail.com>
2021-02-10 23:48:08 +0530

    added card template

--- MainScreen.js
+++ MainScreen.js
@@ -12,12 +12,12 @@ import {
   TextInput
 } from 'react-native';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
-
+import LinearGradient from 'react-native-linear-gradient';
 
 
 const screenWidth = Dimensions.get('window').width;
 const screenHeight = Dimensions.get('window').height;
-
+const CARD_WIDTH = (screenWidth * 0.6);
 
 const MainScreen = () =>  {
 
@@ -30,16 +30,43 @@ const MainScreen = () =>  {
         backgroundColor="#ffffff00"
       />
       <MapView
-       provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-       style={styles.map}
-       region={{
-         latitude: 37.78825,
-         longitude: -122.4324,
-         latitudeDelta: 0.015,
-         longitudeDelta: 0.0121,
-       }}
-     >
-     </MapView>
+        provider={PROVIDER_GOOGLE} 
+        style={styles.map}
+        region={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        }}
+        
+      >
+      </MapView>
+
+
+      <View style={styles.searchBarContainer} />
+      {/* <LinearGradient 
+        style={styles.bottomGrad}
+        colors={['#ffffff00', '#ffffff75','#ffffff']}
+      /> */}
+      <View 
+        style={styles.cardsViewContainer}
+      >
+        <ScrollView 
+          horizontal 
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{
+            paddingRight: (screenWidth - (screenWidth * 0.9)) / 2
+          }}
+          snapToInterval={ (screenWidth * 0.5)  }
+          snapToAlignment="center"
+
+        >
+          <View style={styles.mainCard} />
+          <View style={styles.mainCard} />
+          <View style={styles.mainCard} />
+          <View style={styles.mainCard} />
+        </ScrollView>
+      </View>
     </View>
   )
 }
@@ -54,6 +81,45 @@ const styles = StyleSheet.create({
   map: {
     ...StyleSheet.absoluteFillObject,
   },
+  searchBarContainer: {
+    width: screenWidth * 0.9,
+    height: 60,
+    position: 'absolute',
+    zIndex: 10,
+    backgroundColor: '#fff',
+    top: StatusBar.currentHeight + ((screenWidth - (screenWidth * 0.9)) / 2),
+    marginLeft: (screenWidth - (screenWidth * 0.9)) / 2,
+    marginRight: (screenWidth - (screenWidth * 0.9)) / 2,
+    elevation: 8,
+    borderRadius: 8
+  },
+  bottomGrad: {
+    width: screenWidth,
+    height: 300,
+    position: 'absolute',
+    bottom: 0,
+    zIndex: 1
+  },
+  cardsViewContainer: {
+    // backgroundColor: 'transparent',
+    // backgroundColor: 'red',
+    width: screenWidth,
+    height: 'auto',
+    position: 'absolute',
+    bottom: 0,
+    zIndex: 2,
+    paddingBottom: (screenWidth - (screenWidth * 0.9)) / 2
+  },
+  mainCard: {
+    height: 300,
+    width: CARD_WIDTH,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    marginLeft: (screenWidth - (screenWidth * 0.9)) / 2,
+    borderWidth: 1,
+    borderColor: '#eee',
+    elevation: 8
+  }
 })
 
 export default MainScreen;
