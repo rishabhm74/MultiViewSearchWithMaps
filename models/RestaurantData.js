4dbc8a0d6612ea3131d1040f912205124bfb144a
rishabh <rishabhm1974@gmail.com>
2021-02-13 16:14:28 +0530

    added restaurant data and completed list view cards

---
+++ models/RestaurantData.js
@@ -0,0 +1,76 @@
+const RestaurantData = [
+  {
+    restaurantId: 1,
+    restaurantName: 'Dil Se Re Restaurant',
+    restaurantData: {
+      restaurantImgUrl: require('./assets/images/restaurants/1.jpg'),
+      restaurantSpecialities: [ 'Gujarati', 'Chinese', 'Madwadi', 'Maharastrian' ],
+      restaurantRating: 3,
+      distanceToRestaurant: '1.85 Km',
+      currentState: 'Open',
+      closingTime: '9'
+    }
+  },
+  {
+    restaurantId: 2,
+    restaurantName: 'Dil Se Re Restaurant',
+    restaurantData: {
+      restaurantImgUrl: require('./assets/images/restaurants/2.jpg'),
+      restaurantSpecialities: [ 'Gujarati', 'Madwadi', 'MarMaharastrianathi', 'Rajasthani' ],
+      restaurantRating: 5,
+      distanceToRestaurant: '1.05 Km',
+      currentState: 'Closed',
+      closingTime: '6'
+    }
+  },
+  {
+    restaurantId: 3,
+    restaurantName: 'Dil Se Re Restaurant',
+    restaurantData: {
+      restaurantImgUrl: require('./assets/images/restaurants/3.jpg'),
+      restaurantSpecialities: [ 'Madwadi', 'Rajasthani' ],
+      restaurantRating: 4,
+      distanceToRestaurant: '2.3 Km',
+      currentState: 'Open',
+      closingTime: '9'
+    }
+  },
+  {
+    restaurantId: 4,
+    restaurantName: 'Dil Se Re Restaurant',
+    restaurantData: {
+      restaurantImgUrl: require('./assets/images/restaurants/4.jpg'),
+      restaurantSpecialities: [ 'Jain', 'Gujarati', 'Continental', 'Inter-Continental' ],
+      restaurantRating: 4,
+      distanceToRestaurant: '0.5 Km',
+      currentState: 'Open',
+      closingTime: '8'
+    }
+  },
+  {
+    restaurantId: 5,
+    restaurantName: 'Dil Se Re Restaurant',
+    restaurantData: {
+      restaurantImgUrl: require('./assets/images/restaurants/5.jpg'),
+      restaurantSpecialities: [ 'Soth-Indian', 'Sri-Lankan', 'Chinese' ],
+      restaurantRating: 2,
+      distanceToRestaurant: '2.7 Km',
+      currentState: 'Closed',
+      closingTime: '9'
+    }
+  },
+  {
+    restaurantId: 6,
+    restaurantName: 'Dil Se Re Restaurant',
+    restaurantData: {
+      restaurantImgUrl: require('./assets/images/restaurants/6.jpg'),
+      restaurantSpecialities: [ 'Gujarati', 'Sindhi' ,'Rajasthani' ],
+      restaurantRating: 4,
+      distanceToRestaurant: '0.3 Km',
+      currentState: 'Closed',
+      closingTime: '8:30'
+    }
+  }
+];
+
+export default RestaurantData;
